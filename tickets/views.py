7840c4f075cfa2f58fe4d2c9754bf56a16e64581
diegocode <diegocode@gmail.com>
2022-04-25 10:31:50 -0300

    corregido comentario

--- tickets/views.py
+++ tickets/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 import datetime
 
-# vista para probar urls
+# vista para probar
+# copiada de la documentación de django:
 # https://docs.djangoproject.com/en/4.0/topics/http/views/
 def current_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
+
